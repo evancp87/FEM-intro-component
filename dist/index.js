688d05bd43ba5f8b34ca17e18923d6893d4aa986
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-07-17 19:55:02 +0100

    dist folder holding files generated by gulp

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+/* eslint-disable indent */
+
+// handles both accordions for mobile and dropdown menus on desktop
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("active");
+  });
+});
+
+// hamburger menu/sidebar for mobiles
+
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+
+// handles toggling of dark overlay when sidebar is active
+const bodyOverlay = document.querySelector(".main-content__body-overlay");
+
+function darkenBackground() {
+  bodyOverlay.classList.toggle("active-body-overlay");
+}
+
+// toggles sidebar open/closed
+function toggleOpenSideBar() {
+  const bar = document.querySelector(".bar");
+  const closeMenu = document.querySelector(".close-menu");
+  bar.classList.toggle("active");
+  closeMenu.classList.toggle("active");
+}
+
+// onClick event for sidebar, invoking overlay and open sidebar
+hamburger.addEventListener("click", () => {
+  sidebar.classList.toggle("active-sidebar");
+  darkenBackground();
+  toggleOpenSideBar();
+});
+//# sourceMappingURL=index.js.map
