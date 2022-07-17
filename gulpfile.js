c94aca2901efce48f03ffc5807515d2caf881c6c
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-07-17 19:25:58 +0100

    refactoring of gulp file to improve accuracy

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,98 @@
 /* eslint-disable indent */
 /* eslint-disable no-undef */
-const gulp = require("gulp"),
-  sass = require("gulp-sass")(require("sass")),
-  browserSync = require("browser-sync").create();
+
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
 
 // compile scss to css
 
 function generateCSS() {
   // where my scss files are generated
   return (
-    gulp
-      .src("./style/**/*.scss")
+    src("./style/**/*.scss", { sourcemaps: true })
       //   pass file through sass compiler
       .pipe(sass().on("error", sass.logError))
+      .pipe(postcss([cssnano()]))
       //   where do i save compiled sass
-      .pipe(gulp.dest("./css"))
+      .pipe(dest("dist", { sourcemaps: "." }))
       //   for stream changes for all browsers
       .pipe(browserSync.stream())
   );
 }
 
-function watch() {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-    },
-  });
-  gulp.watch("./style/**/*.scss", generateCSS);
-  // .on is on change
-  gulp.watch("*.html").on("change", browserSync.reload);
-  gulp.watch("./scripts/*.js").on("change", browserSync.reload);
+function generateJS() {
+  return src("./scripts/*.js", { sourcemaps: true }).pipe(
+    dest("dist", { sourcemaps: "." })
+  );
 }
 
-exports.generateCSS = generateCSS;
-exports.watch = watch;
+function browserSyncServe(cb) {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  cb();
+}
+
+function browserSyncReload(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watchTask() {
+  watch("*.html").on("change", browserSyncReload);
+  watch(
+    ["./scripts/*.js", "./style/**/*.scss"],
+    series(generateCSS, generateJS, browserSyncReload)
+  );
+}
+
+exports.default = series(
+  generateCSS,
+  generateJS,
+  browserSyncServe,
+  browserSyncReload,
+  watchTask
+);
+
+// const gulp = require("gulp"),
+//   sass = require("gulp-sass")(require("sass")),
+//   browserSync = require("browser-sync").create();
+
+// // compile scss to css
+
+// function generateCSS() {
+//   // where my scss files are generated
+//   return (
+//     gulp
+//       .src("./style/**/*.scss")
+//       //   pass file through sass compiler
+//       .pipe(sass().on("error", sass.logError))
+//       //   where do i save compiled sass
+//       .pipe(gulp.dest("./css"))
+//       //   for stream changes for all browsers
+//       .pipe(browserSync.stream())
+//   );
+// }
+
+// function watch() {
+//   browserSync.init({
+//     server: {
+//       baseDir: "./",
+//     },
+//   });
+//   gulp.watch("./style/**/*.scss", generateCSS);
+//   // .on is on change
+//   gulp.watch("*.html").on("change", browserSync.reload);
+//   gulp.watch("./scripts/*.js").on("change", browserSync.reload);
+// }
+
+// exports.generateCSS = generateCSS;
+// exports.watch = watch;
 
 // // gulp-eslint
 // // gulp-awspublish
